05bfd5654df573354be5ee6de96c99b07d8730b4
Travis Lloyd <travis.t.lloyd1996@gmail.com>
2021-10-02 20:54:48 -0600

    added randomization for password

--- Develop/script.js
+++ Develop/script.js
@@ -4,51 +4,66 @@ var includeLower;
 var includeUpper;
 var includeSpecial;
 var includeNums;
+var tempPassword;
 
-var passwordCriteria = [numOfChars, includeLower, includeUpper, includeSpecial, includeNums];
+//var passwordCriteria = [numOfChars, includeLower, includeUpper, includeSpecial, includeNums];
 
 var lowerList = "abcdefghijklmnopqrstuvwxyz";
-var upperlist = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var upperList = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var specialList = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 var numsList = "0123456789";
 
 
 //create prompts for password criteria
 function generatePassword() {
+  tempPassword = "";
   buildCriteria();
 
-  for(i=1;i<=numOfChars;i++) {
-
+  //Checks that at least one of the criterias were used.
+  if(includeLower === false && includeSpecial === false && includeSpecial === false && includeNums === false) {
+    window.alert("You must incluse at least one of the following: numerics, upper-case characters, lower-case characters, special characters.");
+    buildCriteria();
+  };
+  //generate the password.
+  if(includeLower) {
+    tempPassword += lowerList[Math.floor(Math.random() * (lowerList.length - 1))];
   }
+  if(includeSpecial) {
+    tempPassword += specialList[Math.floor(Math.random() * (specialList.length - 1))];
+  }
+  if(includeUpper) {
+    tempPassword += upperList[Math.floor(Math.random() * (upperList.length - 1))];
+  }
+  if(includeNums) {
+    tempPassword += numsList[Math.floor(Math.random() * (numsList.length - 1))];
+  }
+
+  passwordHelper();
+  randomize();
+  return tempPassword;
 }
 
+//determines users perferences for password.
 function buildCriteria() {
-  for(i=0; i<passwordCriteria.length;i++){
-    if (i === 0){
       var length = window.prompt("How many characters long, type a number '8'-'128'");
-      if(length > 8 && length < 128){
+      if(length >= 8 && length <= 128){
         window.alert("Your password will be " + length + " characters long.")
-        passwordCriteria[i] = length;
+        numOfChars = length;
       }else{
         checkLength(length);
       }
       
-    }else if (i === 1){
       includeLower = window.confirm("Would you like to include lower-case characters?");
       lowercase(includeLower);
-      passwordCriteria[i] = includeLower;
-    }else if(i === 2){
+    
       includeUpper = window.confirm("Would you like to include Upper-case Characters?");
       uppercase(includeUpper);
-      passwordCriteria[i] = includeUpper;
-    }else if(i === 3){
+
       includeSpecial = window.confirm("Would you like to include special characters?");
       specialChars(includeSpecial);
-      passwordCriteria[i] = includeSpecial;
-    }else if(i === 4){
+
       includeNums = window.confirm("Would you like to include numeric characters?");
-    }
-  }
+      numericChars(includeNums);
 }
 
 //checks the length of character is in the valid window.
@@ -59,8 +74,6 @@ function checkLength(length){
  } else if (length < 8){
    window.alert("you have requested too few charactres, password can only be 8-128 charactrs long");
    buildCriteria();
- }else{
-   return length;
  }
 }
 
@@ -94,6 +107,16 @@ function specialChars(){
   }
 }
 
+//checks and re-confirms if the user wants numeric chars.
+function numericChars(){
+  if(includeNums) {
+    window.alert("Your password will include numeric characters.");
+    return true;
+  } else {
+    return finalConfirm();
+  }
+}
+
 //does final confirmation.
 function finalConfirm() {
   var youSure = window.confirm("Confirming, means you desire lower security, cancel to increase secruity");
@@ -105,11 +128,54 @@ function finalConfirm() {
   }
 }
 
+//helps create rest of password based of length desired
+function passwordHelper() {
+
+  while (tempPassword.length < parseInt(numOfChars)) {
+    switch (Math.floor(Math.random() * 3)) {
+      case 0:
+        if(includeLower) {
+          tempPassword += lowerList[Math.floor(Math.random() * (lowerList.length - 1))];
+        }
+        break;
+      case 1:  
+        if(includeSpecial) {
+          tempPassword += specialList[Math.floor(Math.random() * (specialList.length - 1))];
+        }
+        break;
+      case 2: 
+        if(includeUpper) {
+          tempPassword += upperList[Math.floor(Math.random() * (upperList.length - 1))];
+        }
+        break;
+      case 3:  
+        if(includeNums) {
+          tempPassword += numsList[Math.floor(Math.random() * (numsList.length - 1))];
+          console.log(tempPassword);
+        }
+        break;
+    }
+
+  }
+}
+
+//after creating the string this function will randomize 
+function randomize() {
+  for(i=0;i<50;i++){
+    var rand1 = Math.floor(Math.random() * (numOfChars - 1));
+    var rand2 = Math.floor(Math.random() * (numOfChars - 1));
+    var tempChar = tempPassword[rand2];
+    tempPassword[rand2] = tempPassword[rand1];
+    tempPassword[rand1] = tempChar;
+  }
+}
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
